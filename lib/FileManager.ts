16c2ed545afef4b9becd6dcd750b7c9c15bbebbc
AyubsWrld <ayubfmohammed@gmail.com>
2025-02-11 22:49:50 -0700

    loading files && upload trigger ui logic , must add handle file logic

--- lib/FileManager.ts
+++ lib/FileManager.ts
@@ -1,9 +1,10 @@
 import { launchCamera , launchImageLibrary } from "react-native-image-picker" ; 
 
 
-enum FILE_ERROR { 
+export enum FILE_ERROR { 
   FILE_SUCCESS = 0 , 
   USER_CANCEL  = 1 , 
+  RESP_ERROR   = 2 , 
 }
 
 type ErrorMessage = { 
@@ -107,6 +108,7 @@ class ImageFactory implements FileFactory{
   }
 }
 
+
 /* @signature : getFileCategory( value : string ) : string 
  * @params    : String with file identifier to split 
  * @purpose   : Derive file type from string 
@@ -114,19 +116,57 @@ class ImageFactory implements FileFactory{
 */
 
 
+
 const getFileCategory = ( value : string ) : string => {
 	return value.split("/")[0] 
+}
+
+const createFileObject = ( metadata : any ) => {
+	let file : FileContainer ; 
+	const fileType = getFileCategory(metadata.type) 
+	if(fileType && fileType == "video"){
+		const factory = new VideoFactory() ; 
+		file = factory.createFile(
+			metadata.fileName ,
+		       	metadata.fileSize ,
+			{ width : metadata.width , height : metadata.height } ,
+		       	metadata.uri , 
+			metadata.type.split("/")[1] , 
+			metadata.duration  , 
+		) ; 
+	}
+	else if(fileType && fileType == "image"){
+		const factory = new ImageFactory() ; 
+		file = factory.createFile(
+			metadata.fileName ,
+		       	metadata.fileSize ,
+			{ width : metadata.width , height : metadata.height } ,
+		       	metadata.uri , 
+			metadata.type.split("/")[1] , 
+		) ; 
+	}
+	return file ; 
+}
+
+/* @signature : handleFile( metadata : object / any ) : FILE_ERROR
+ * @params    : object containing file metadata 
+ * @purpose   : write file contents to db 
+ * @return    : SUCCESS if ok , some FILE_ERROR if something wrong occurs 
+*/
+
+const handleFile = ( metadata : any ) => { 
+      const file = createFileObject(metadata) ; 
+      console.log(file) ; 
 }
 
 /*
  * @params  : None 
  * @purpose : Allows user to open and select image of their choice 
  * @return  : FILE_ERROR 
- * 
- *
 */
 
-export const openImagePicker = () => {
+export const openImagePicker = async (): Promise<FILE_ERROR> => {
+  return new Promise((resolve) => {
     const options = {
       mediaType: 'any',
       includeBase64: false,
@@ -136,20 +176,20 @@ export const openImagePicker = () => {
 
     launchImageLibrary(options, (response) => {
       if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('Image picker error: ', response.error);
+        resolve(FILE_ERROR.USER_CANCEL);
+      } else if (response.error || !response || !response.assets[0]) {
+        resolve(FILE_ERROR.RESP_ERROR);
       } else {
-	      const fileType = getFileCategory(response.assets[0].type) ; 
-	      if(fileType){
-		      if(fileType == "video"){
-			      console.log("Handling Video") 
-		      }
-
-		      else if(fileType == "image"){
-			      console.log("Handling image") 
-		      }
-	      }
+        handleFile(response.assets[0]); // Does this also have to be async 
+        resolve(FILE_ERROR.FILE_SUCCESS);
       }
     });
-  };
+  });
+};
+
+
+export const openImagePicker = async () : Promise<FILE_ERROR> => {
+  return new Promise((resolve) => {
+    console.log("IT worked !") ; 
+  });
+}
--- src/screens/ProfileScreen.tsx
+++ src/screens/ProfileScreen.tsx
@@ -1,11 +1,30 @@
 import { View, Text, Button } from "react-native"
+import React , { useState } from "react"
 import { NativeStackScreenProps } from "@react-navigation/native-stack"
 import { AppStackParamList } from "../navigation/AppNavigator"
-import { openImagePicker } from "../../lib/FileManager"
+import { FILE_ERROR , openImagePicker  , writeFile } from "../../lib/FileManager"
 
 type Props = NativeStackScreenProps<AppStackParamList, "Profile">
 
+
+
+
 export function ProfileScreen({ navigation }: Props) {
+  const [ isSelected ,  setIsSelected ] = useState<boolean>(false); 
+
+  const handleImage = async () => {
+    const exitCode : FILE_ERROR = await openImagePicker() ; 
+    if(!exitCode) console.log("undef") ; 
+    if(exitCode == FILE_ERROR.FILE_SUCCESS){
+      console.log("Succesfully selected the file") ; 
+      setIsSelected(true) ; 
+    }
+  }
+
+  const writeFile = async () => {
+    const exitCode = await writeFile() ; 
+  }
+
   return (
     <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
       <Text>Profile Screen</Text>
@@ -16,8 +35,13 @@ export function ProfileScreen({ navigation }: Props) {
 
       <Button
         title="Select Image"
-        onPress={() => openImagePicker()} 
+        onPress={() => handleImage()} 
       />
+
+      { isSelected && <Button
+        title="Upload Image"
+        onPress={() => handleUpload()} 
+      />}
     </View>
   )
 }
