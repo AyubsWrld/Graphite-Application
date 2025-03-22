76df0fa2d1f8e0430602396b6ed43e1288f2001f
unknown <ayubfmohammed@gmail.com>
2025-03-22 14:48:46 -0600

    more changes

---
+++ utils/database/entities/File.ts
@@ -0,0 +1,28 @@
+import { Entity, Column, PrimaryColumn } from "typeorm";
+
+@Entity()
+export class Image {
+    @PrimaryColumn({ type: "text" })
+    abs_path: string;
+
+    @Column({ type: "text" })
+    filename: string;
+
+    @Column({ type: "integer" })
+    height: number;
+
+    @Column({ type: "integer" })
+    width: number;
+
+    @Column({ type: "text" })
+    extension: string;
+
+    @Column({ type: "text" })
+    filetype : string;
+
+    @Column({ type: "text" })
+    duration : string;
+
+    @Column({ type: "text" })
+    uri: string;
+}
